4b2e2e723f5c8b104ecc97683060754bc6e5a0d7
FoseFx <fosefx@pm.me>
2019-08-20 12:37:29 +0200

    Fixed Linting problems

--- content_scripts/faceit_event_binding.spec.ts
+++ content_scripts/faceit_event_binding.spec.ts
@@ -10,7 +10,7 @@ it('should emit event, when onpopstate is called', (next) => {
 });
 it('should emit event, when JS pushes new State to history', (next) => {
     ecl_route_inject();
-    document.addEventListener('ecl_report_addon_route_change', (e:CustomEvent) => {
+    document.addEventListener('ecl_report_addon_route_change', (e: CustomEvent) => {
         expect(e.detail).toBe('http://localhost/lol?lmao=false&true');
         next();
     });
