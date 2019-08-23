538fdb0a0e5a506bb6c04087b19506a3d8a5c006
FoseFx <fosefx@pm.me>
2019-08-23 21:23:24 +0200

    Fixed one problem, next problem found ...

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 rm -rf dist && # clean old dist folder
 mkdir dist && # make new dist folder
-tsc -b tsconfig.content_scripts.json && # transpile content_scripts and msg_broker
+tsc -b tsconfig.content_scripts.$1.json && # transpile content_scripts and msg_broker
 rm -rf dist/form && # we dont need this
 sed -i 's/export //' dist/content_scripts/faceit.js &&# remove 'export' from file
 sed -i 's/export //' dist/background/msg_broker.js && # x2
 cp manifest.json dist/. && # copy manifest over
-npm run build:build_vue # build form
+cd form && 
+npm run build && # build form
+cp -r dist/js/* ../dist/content_scripts && # copy files to the right place
+cp dist/css/* ../dist/content_scripts &&
+cd ..
