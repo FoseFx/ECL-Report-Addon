ff3c1e450a2d696d0e6a0ee04897fc1d4a325e90
FoseFx <fosefx@pm.me>
2019-10-22 23:21:09 +0200

    Adds e2e to pre-push

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -1,9 +1,12 @@
 const run = require('./run');
+const arg = process.argv[2];
 
 
 (async () => {
-  console.log('\n\x1b[32m%s\x1b[0m', 'Preparing End to End Tests');
-  await run('npm run build:dev');
+  console.log('\n\x1b[32m%s\x1b[0m', 'Preparing End to End Tests');  
+  if (arg !== '--no-build') {
+    await run('npm run build:dev');
+  }
   process.chdir('dist');
   await run('zip -r addon.xpi *');
   process.chdir('../');
