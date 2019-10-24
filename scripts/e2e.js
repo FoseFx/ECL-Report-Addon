66afa2174d8607bc96e5ca0f3a1be5af52d211e1
FoseFx <fosefx@pm.me>
2019-10-24 12:01:59 +0200

    Adds e2e to windows workflow

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -1,14 +1,27 @@
+const path = require('path');
 const run = require('./run');
 const arg = process.argv[2];
+
+async function compressAndRename() {
+  if (process.platform === 'win32') {
+    const distPath = path.join(__dirname, './dist');
+    const addonPath = path.join(__dirname, './dist/addon');
+    await run(`Compress-Archive -Path ${distPath}\\* -CompressionLevel Fastest -DestinationPath ${addonPath}`);
+    await run(`rename ${addonPath}.zip ${addonPath}.xpi`);
+    await run(`dir`);
+  } else {
+    process.chdir('dist');
+    await run('zip -r addon.xpi *');
+    process.chdir('../');
+  }
+}
 
 (async () => {
   console.log('\n\x1b[32m%s\x1b[0m', 'Preparing End to End Tests');  
   if (arg !== '--no-build') {
     await run('npm run build:dev');
   }
-  process.chdir('dist');
-  await run('zip -r addon.xpi *');
-  process.chdir('../');
+  await compressAndRename();
   await run('docker build -t ecl-proxy e2e/proxy');
   await run('docker run --rm --name ecl-proxy -p 8888:8080 -d ecl-proxy');
   console.log('\x1b[32m%s\x1b[0m', 'Starting End to End Tests');
