ae457bc3494dd7efcce6e8d84765011528a75266
FoseFx <fosefx@pm.me>
2019-10-25 15:16:12 +0200

    plsplspls

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -17,11 +17,9 @@ async function compressAndRename() {
 }
 async function buildProxy() {
   if (process.platform === 'win32') {
-    await run('docker build -t ecl-proxy -f e2e\\proxy\\windows.Dockerfile e2e\\proxy');
+    await run('docker -SwitchDaemon');
   }
-  else {
-    await run('docker build -t ecl-proxy e2e/proxy');
-  }
+  await run('docker build -t ecl-proxy e2e/proxy');
 }
 
 (async () => {
