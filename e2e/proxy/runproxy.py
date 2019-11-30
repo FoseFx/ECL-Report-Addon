f29ac15a5f237aa9e8ff9e1e320552b71aa51260
FoseFx <fosefx@pm.me>
2019-11-30 17:38:10 +0100

    Started investigating faceit backend

--- e2e/proxy/runproxy.py
+++ e2e/proxy/runproxy.py
@@ -1,4 +1,28 @@
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow):
-    pass
+    request = flow.request
+    host_name = request.pretty_host
+    if is_captcha(request):
+        print("FF: CAPTCHA request")
+        print_text_of_req(request)
+    if is_faceit_host(host_name):
+        print("FF: url: " + request.pretty_url)
+        print_text_of_req(request)
+        if "https://api.faceit.com/auth/v1/oauth/token" == request.pretty_url:
+            flow.response = http.HTTPResponse.make(
+            418, b"I'm a teapot",
+        )
+
+def is_faceit_host(hostname):
+    return "api.faceit.com" in hostname
+
+def is_captcha(request):
+    return "https://www.google.com/recaptcha/api2/userverify?k=" in request.pretty_url
+
+def print_text_of_req(request):
+    try:
+        print("FF: text: " + request.get_text)
+    except ValueError as e:
+        print("ff: ValueError while decoding response: ")
+        print(e)
